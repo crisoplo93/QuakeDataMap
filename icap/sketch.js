a04ba6be9de898f455aecb1787c0acc3275232c8
crisoplo93 <cris_93_bep@hotmail.com>
2017-02-17 02:46:26 +0300

    Update sketch.js

--- icap/sketch.js
+++ icap/sketch.js
@@ -1,1 +1,25 @@
+var pics;
 
+function preload()
+{
+  pics= loadImage('foto.jpg');
+}
+
+
+function setup() {
+ createCanvas(window.innerWidth,window.innerHeight);
+Hpage();
+
+
+}
+
+
+function Hpage()
+{
+ background(128,0,128);
+ image(pics,window.innerWidth/2,window.innerHeight/2);  
+ curtxt=createP('Эн мин сурэхпэр');
+ curtxt.position(window.innerWidth/2,window.innerHeight-50);
+ curtxt.style("color","#ffffff");
+ curtxt.style("font","bold 16px arial, sans-serif"); 
+}
